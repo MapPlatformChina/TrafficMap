6400f41ad619b45a01a3d8e4350cac8dc600360f
Steve Wang <stevewang@outlook.com>
2014-04-13 15:14:54 +0800

    update basemap and linkmatch

--- Code/BaseMap.py
+++ Code/BaseMap.py
@@ -5,7 +5,7 @@
 # This file is one of traffic map utility component files
 # Created in 2013
 # ######################################################################
-from pos_vs_coords import *
+from PosCoordsUtl import *
 from Tkinter import *
 from PIL import Image, ImageTk
 from StringIO import StringIO
@@ -14,89 +14,102 @@ import urllib2
 
 # Below is the area for class definition
 class BaseMap:
-	def __init__(self):
-		self.rows = 3
-		self.cols = 4
-		self.image_size = 256
-		self.photo_array = []
-		self.default_image = None
-		self.zoom_level = 7
-		self.center_coords = [116,39]
-		self.tile_shift = [0,0]
-		self.reset_tiles()
+	def __init__(self, canvas_size, image_size = 256, center = [116,39]):
+		self.canvas_size = canvas_size
+		self.image_size = image_size
+		
+		self.rows = int(math.ceil(self.canvas_size[1] / float(self.image_size)))
+		self.cols = int(math.ceil(self.canvas_size[0] / float(self.image_size)))
+		
+		self.zoom_level = 10
+		self.pos_coords_utl = PosCoordsUtl(self.zoom_level, self.canvas_size, self.image_size)
+		
+		self.center_coords = center
+
+		# below are attributes of display
+		self.default_tile = None
 		self.cached_tiles = []
 
-	def draw(self, canvas):
-		self.set_tile_position()
-		for i in range(0,self.rows + 1):
-			for j in range(0,self.cols + 1):
-				index = i * (self.cols + 1) + j
-				x = self.image_size/2 + 256 * j - self.tile_shift[0]
-				y = self.image_size/2 + 256 * i - self.tile_shift[1]
-				if self.photo_array[index] != None:
-					canvas.create_image(x,y,image = self.photo_array[index])
-				else:
-					canvas.create_image(x,y,image = self.default_image)
-		canvas.create_rectangle(0,0,1024,768)
+		self.tile_shift = [0,0]
+		
+		self.cache_tile()
+	
+	def set_center(self, center_coords):
+		self.center_coords = center_coords
+		#self.cache_tile()
+		
+	def set_zoom_level(self, zoom_level):
+		self.zoom_level = zoom_level
+		self.pos_coords_utl.set_zoom_level(self.zoom_level)
+		#self.cache_tile()
 
-	def move_center(self, movement):
-		pos = geocoordinates_to_pixels(self.center_coords, self.zoom_level, self.image_size)
-		new_pos = [pos[0] - movement[0], pos[1] - movement[1]]
-		print pos, new_pos, self.center_coords,
-		self.center_coords = pixels_to_geocoordinates(new_pos, self.zoom_level, self.image_size)
-		print self.center_coords
-		self.update_tiles(movement)
-		return self.center_coords
-	
-	def set_tile_position(self):
-		# calculate shifting of the whole picture
-		pos = geocoordinates_to_pixels(self.center_coords, self.zoom_level, self.image_size)
-		left_top_pos = [ pos[0] - self.cols/2.0 * self.image_size , pos[1] - self.rows/2.0 * self.image_size]
-		self.tile_shift = [left_top_pos[0] % self.image_size, left_top_pos[1] % self.image_size]
-	
-	def update_tiles(self,movement):
-		grids = get_conner_grid(self.center_coords, self.zoom_level, self.image_size)
+	def cache_tile(self):
+		# check out left top and right bottom grid first
+		grids = self.pos_coords_utl.get_conner_grid(self.center_coords)
+		
 		left_top_grid = grids[0]
 
 		temp_photo_array = []
 		temp_cached_tiles = []
-		for i in range(0, self.rows + 1):
-			for j in range(0, self.cols + 1):
+		for i in range(0, self.rows + 1):	#cache one more tile to make sure the map is fully covered
+			for j in range(0, self.cols + 1):	#cache one more tile to make sure the map is fully covered
 				x = left_top_grid[0] + j
 				y = left_top_grid[1] + i
 				
 				# fetch new tile only in case not cached
-				tk_photo = self.get_from_cache(x,y)
-				if tk_photo == None:
+				if not self.is_tile_in_cache(x,y):
 					tk_photo = self.get_image_from_server((x,y))
+					self.cached_tiles.append((x,y,self.zoom_level,tk_photo))
 				
-				temp_photo_array.append(tk_photo)
-				temp_cached_tiles.append((x,y))
-		
-		self.photo_array = temp_photo_array
-		self.cached_tiles = temp_cached_tiles
-	
-	def get_from_cache(self,x,y):
-		for i in range(0,len(self.cached_tiles)):
-			if self.cached_tiles[i][0] ==  x and self.cached_tiles[i][1] == y:
-				return self.photo_array[i]
+	def is_tile_in_cache(self, x, y):
+		for tile_item in self.cached_tiles:
+			if tile_item[0] == x and tile_item[1] == y and tile_item[2] == self.zoom_level:
+				return True
+		return False
+
+	def get_tile(self, x, y):
+		for tile_item in self.cached_tiles:
+			if tile_item[0] == x and tile_item[1] == y and tile_item[2] == self.zoom_level:
+				return tile_item[3]
 		return None
-	
-	def reset_tiles(self):
-		grids = get_conner_grid(self.center_coords, self.zoom_level, self.image_size)
+		
+	def optimize_cache(self):
+		pass	#to be added
+		
+	def draw(self, canvas, mark_point_array = []):
+		self.cache_tile()
+		canvas.delete(ALL)
+		self.tile_shift = self.pos_coords_utl.get_shift(self.center_coords)
+		grids = self.pos_coords_utl.get_conner_grid(self.center_coords)
 		left_top_grid = grids[0]
-		self.photo_array = []
-		self.cached_tiles = []
-		print 'Start to fetching map tiles ...',
-		for i in range(0,self.rows + 1):		# cache one more photo to display
-			for j in range(0,self.cols + 1):	# cache one more photo to display
-				index = i * (self.cols + 1) + j
+		
+		for i in range(0,self.rows + 1):
+			for j in range(0,self.cols + 1):
 				x = left_top_grid[0] + j
 				y = left_top_grid[1] + i
-				self.photo_array.append(self.get_image_from_server((x,y)))
-				self.cached_tiles.append((x,y))
-		print 'Done!'		
-					
+				image_obj = self.get_tile(x,y)
+				
+				pos_x = self.image_size/2 + 256 * j - self.tile_shift[0]
+				pos_y = self.image_size/2 + 256 * i - self.tile_shift[1]
+				
+				if image_obj != None:
+					canvas.create_image(pos_x,pos_y, image=image_obj)
+				else:
+					print x,y, 'is not loaded!'
+		
+		for mark_point in mark_point_array:
+			if not ('last_mark_point' in locals()):
+				last_mark_point = mark_point
+				last_pos = self.get_pos(last_mark_point)
+				continue
+			print last_mark_point,mark_point
+			pos = self.get_pos(mark_point)
+			
+			canvas.create_line(last_pos[0], last_pos[1],
+								pos[0], pos[1],
+								fill = 'blue', width = 3)
+			last_mark_point = mark_point
+			last_pos = pos
 			
 	def get_center_coords(self):
 		return self.center_coords
@@ -107,7 +120,7 @@ class BaseMap:
 		request_part2 = '/%d/%d/%d/256/png8' % (self.zoom_level, grid[0], grid[1])
 		request_part3 = '?app_id=demo_qCG24t50dHOwrLQ&token=NYKC67ShPhQwqaydGIW4yg&lg=chi'
 		request_string = request_part1 + request_part2 + request_part3
-		print request_string
+		#print request_string
 		
 		# generate request for the image
 		req = urllib2.Request(request_string)
@@ -118,44 +131,22 @@ class BaseMap:
 		pil_image = Image.open(StringIO(image_data))
 		tk_photo = ImageTk.PhotoImage(pil_image)
 		return tk_photo
-	
-	def config(self, center_coords, zoom_level, rows=3, cols=4):
-		self.center_coords = center_coords
-		self.zoom_level = zoom_level
-		self.rows = rows
-		self.cols = cols
-		self.reset_tiles()
-	
-	def get_coords_box(self):
-		pos = geocoordinates_to_pixels(self.center_coords, self.zoom_level, self.image_size)
-		left_top_pos = [ pos[0] - self.cols/2.0 * self.image_size , pos[1] - self.rows/2.0 * self.image_size]
-		right_bottom_pos = [ pos[0] + self.cols/2.0 * self.image_size , pos[1] + self.rows/2.0 * self.image_size]
-		left_top_coords = pixels_to_geocoordinates(left_top_pos, self.zoom_level, self.image_size)
-		right_bottom_coords = pixels_to_geocoordinates(right_bottom_pos, self.zoom_level, self.image_size)
-		return (left_top_coords[0],left_top_coords[1],right_bottom_coords[0],right_bottom_coords[1])
-		
-		
-	
-#	def update(self):
 
-#		grids = get_conner_grid(self.center_coords, self.zoom_level, self.image_size)
-#		left_top_grid = grids[0]
-#		self.photo_array = []
-#		print 'Start to fetching map tiles ...',
-#		for i in range(0,self.rows + 1):		# cache one more photo to display
-#			for j in range(0,self.cols + 1):	# cache one more photo to display
-#				index = i * (self.cols + 1) + j
-#				x = left_top_grid[0] + j
-#				y = left_top_grid[1] + i
-#				self.photo_array.append(self.get_image_from_server((x,y)))
-#		print 'Done!'
-		
-		# calculate shifting of the whole picture
-#		pos = geocoordinates_to_pixels(self.center_coords, self.zoom_level, self.image_size)
-#		left_top_pos = [ pos[0] - self.cols/2.0 * self.image_size , pos[1] - self.rows/2.0 * self.image_size]
-		
-#		self.tile_shift = [left_top_pos[0] % self.image_size, left_top_pos[1] % self.image_size]
-		
+	def move_center(self, movement):
+		pos = self.pos_coords_utl.coords_to_pos(self.center_coords)
+		new_pos = [pos[0] - movement[0], pos[1] - movement[1]]
+		print pos, new_pos, self.center_coords,
+		self.center_coords = self.pos_coords_utl.pos_to_coords(new_pos)
+		print self.center_coords
+		self.cache_tile()
+		return self.center_coords
+
+	def get_pos(self, coords):
+		pos = self.pos_coords_utl.coords_to_pos(coords)
+		center_pos = self.pos_coords_utl.coords_to_pos(self.center_coords)
+		diff = [center_pos[0]-pos[0],center_pos[1]-pos[1]]
+		return [self.canvas_size[0]/2 - diff[0], self.canvas_size[1]/2 - diff[1]]
+
 		
 # Below is for helper functions
 
@@ -177,7 +168,7 @@ def main():
 	map_canvas = Canvas(map_area, width = 1024, height = 768)
 	map_canvas.pack()
 	
-	basemap_instance = BaseMap()
+	basemap_instance = BaseMap((1024,768))
 	basemap_instance.draw(map_canvas)
 	
 	root_widget.mainloop()
--- Code/LinkMatch.py
+++ Code/LinkMatch.py
@@ -9,10 +9,18 @@
 # Below is the area for class definition
 import math
 from RoadNetworkV2 import *
+from PosCoordsUtl import *
+
+MAX_DIS = 10**2 + 10**2
 
 class LinkMatch:
+
 	def __init__(self):
-		self.link_match_table = []
+		# { grid: [[direction_sign, latitude, longitude, LCD, direction] ...] }
+		# grid is in format of GRID_GRID, zoom level 
+		self.link_match_table = {}
+		self.zoom_level_of_grids = 15
+
 		self.load()
 	
 	def load(self):
@@ -23,7 +31,17 @@ class LinkMatch:
 			direction_sign, latitude_str, longitude_str, link_name = line.split(';')
 			latitude = float(latitude_str)
 			longitude = float(longitude_str)
-			self.link_match_table.append((direction_sign, latitude, longitude, link_name))
+			if link_name[-1] == '\n':
+				link_name = link_name[0:-1]
+			LCD = link_name[0:-2]
+			direction = int(link_name[-1:])
+			
+			# PosCoordsUtl is longitude first
+			grid = geocoordinates_to_grid((longitude,latitude), self.zoom_level_of_grids)
+			
+			key = ('%d_%d' % (grid[0],grid[1]))
+			self.link_match_table[key] = [direction_sign, latitude, longitude, LCD, direction]
+			
 		file_stream.close()
 		
 		
@@ -31,16 +49,35 @@ class LinkMatch:
 	# para of geo_a/b should be in the format of latitude/longitude
 	def match(self,geo_a, geo_b):
 		direction_sign = get_direction_sign_with_geo(geo_a, geo_b)
-		geo = [(geo_a[0] + geo_b[0]) / 2, (geo_a[1] + geo_b[1]) / 2]
-		min_dis = 10**2 + 10**2
-		link_name = None
-		for link in self.link_match_table:
-			dis = (geo[0] - link[1]) **2 + (geo[1] - link[2])**2 
-			if direction_sign == link[0] and dis < min_dis:
-				link_name = link[3]
-				min_dis = dis
-		return link_name
-	
+		#geo = [(geo_a[0] + geo_b[0]) / 2, (geo_a[1] + geo_b[1]) / 2]
+		geo = geo_b
+		
+		# PosCoordsUtl is longitude first
+		grid = geocoordinates_to_grid((geo[1],geo[0]), self.zoom_level_of_grids)
+
+		grid_start = grid
+		grid_end = grid
+		min_dis = MAX_DIS
+		link_matched = ['?','?']
+		while True:
+			grid_start = [grid_start[0] - 1, grid_start[1] - 1]
+			grid_end = [grid_end[0] + 1, grid_end[1] + 1]
+			for index_lat in range(grid_start[0],grid_end[0] + 1):
+				for index_lon in range(grid_start[1],grid_end[1] + 1):
+					key = ('%d_%d' % (index_lat,index_lon))
+					if not self.link_match_table.has_key(key):
+						continue
+					lat = self.link_match_table[key][1]
+					lon = self.link_match_table[key][2]
+					# calculate distance with latitude zoomed
+					dis = ((geo[0] - lat)*2) **2 + (geo[1] - lon)**2 
+					if direction_sign == self.link_match_table[key][0] and dis < min_dis:
+						link_matched[0] = self.link_match_table[key][3]
+						link_matched[1] = str(self.link_match_table[key][4])
+						min_dis = dis
+			if min_dis < MAX_DIS:
+				return link_matched
+
 def main():
 
 	# Here, your unit test code or main program
