6416df5c1befb3acd0af78acc4dd3ef692381732
Steve Wang <stevewang@outlook.com>
2014-04-13 22:58:18 +0800

    update BaseMap

--- Code/BaseMap.py
+++ Code/BaseMap.py
@@ -21,11 +21,16 @@ class BaseMap:
 		self.rows = int(math.ceil(self.canvas_size[1] / float(self.image_size)))
 		self.cols = int(math.ceil(self.canvas_size[0] / float(self.image_size)))
 		
-		self.zoom_level = 10
+		self.zoom_level = 12
 		self.pos_coords_utl = PosCoordsUtl(self.zoom_level, self.canvas_size, self.image_size)
 		
 		self.center_coords = center
 
+		# hot_spots are for marks that can be dragged around
+		# format: [spot_pos, spot_coords, spot_radius, spot_text]
+		self.hot_spots = []
+		self.selected_spot = None
+		
 		# below are attributes of display
 		self.default_tile = None
 		self.cached_tiles = []
@@ -34,13 +39,19 @@ class BaseMap:
 		
 		self.cache_tile()
 	
+	def update_spots(self):
+		for spot in self.hot_spots:
+			spot[0] = self.get_pos(spot[1])
+	
 	def set_center(self, center_coords):
 		self.center_coords = center_coords
+		self.update_spots()
 		#self.cache_tile()
 		
 	def set_zoom_level(self, zoom_level):
 		self.zoom_level = zoom_level
 		self.pos_coords_utl.set_zoom_level(self.zoom_level)
+		self.update_spots()
 		#self.cache_tile()
 
 	def cache_tile(self):
@@ -83,6 +94,7 @@ class BaseMap:
 		grids = self.pos_coords_utl.get_conner_grid(self.center_coords)
 		left_top_grid = grids[0]
 		
+		# draw tiles
 		for i in range(0,self.rows + 1):
 			for j in range(0,self.cols + 1):
 				x = left_top_grid[0] + j
@@ -97,12 +109,13 @@ class BaseMap:
 				else:
 					print x,y, 'is not loaded!'
 		
+		# draw array in lines
 		for mark_point in mark_point_array:
 			if not ('last_mark_point' in locals()):
 				last_mark_point = mark_point
 				last_pos = self.get_pos(last_mark_point)
 				continue
-			print last_mark_point,mark_point
+			#print last_mark_point,mark_point
 			pos = self.get_pos(mark_point)
 			
 			canvas.create_line(last_pos[0], last_pos[1],
@@ -110,7 +123,16 @@ class BaseMap:
 								fill = 'blue', width = 3)
 			last_mark_point = mark_point
 			last_pos = pos
-			
+		
+		# draw hot spots
+		for spot in self.hot_spots:
+			self.draw_hot_spot(canvas, spot[0], spot[2], spot[3])
+
+	def draw_hot_spot(self, canvas, pos, r, txt):
+		the_circle = canvas.create_oval([pos[0]-r,pos[1]-r,pos[0]+r,pos[1]+r],fill='blue')
+		canvas.create_text(pos[0],pos[1],text=txt, fill='white')
+		return the_circle
+		
 	def get_center_coords(self):
 		return self.center_coords
 		
@@ -136,9 +158,9 @@ class BaseMap:
 		pos = self.pos_coords_utl.coords_to_pos(self.center_coords)
 		new_pos = [pos[0] - movement[0], pos[1] - movement[1]]
 		print pos, new_pos, self.center_coords,
-		self.center_coords = self.pos_coords_utl.pos_to_coords(new_pos)
+		self.set_center(self.pos_coords_utl.pos_to_coords(new_pos))
 		print self.center_coords
-		self.cache_tile()
+		# self.cache_tile()
 		return self.center_coords
 
 	def get_pos(self, coords):
@@ -146,7 +168,58 @@ class BaseMap:
 		center_pos = self.pos_coords_utl.coords_to_pos(self.center_coords)
 		diff = [center_pos[0]-pos[0],center_pos[1]-pos[1]]
 		return [self.canvas_size[0]/2 - diff[0], self.canvas_size[1]/2 - diff[1]]
+	
+	# para of canvas_pos should be pos in current canvas
+	def get_coords(self,canvas_pos):
+		pos_diff = [canvas_pos[0] - self.canvas_size[0] / 2, canvas_pos[1] - self.canvas_size[1] / 2]
+		center_pos = self.pos_coords_utl.coords_to_pos(self.center_coords)
+		pos = [center_pos[0] + pos_diff[0], center_pos[1] + pos_diff[1]]
+		return self.pos_coords_utl.pos_to_coords(pos)
 
+	
+	def add_spot(self, spot_coords, spot_radius, spot_text):
+		spot_pos = self.get_pos(spot_coords)
+		self.hot_spots.append([spot_pos, spot_coords, spot_radius, spot_text])
+
+	def get_spot(self, index):
+		return self.hot_spots[index]
+		
+	# below are added
+	def update_map(self):
+		#	self.center_coords = ((self.from_longitude + self.to_longitude) / 2, (self.from_latitude + self.to_latitude) / 2)
+		self.basemap.set_zoom_level(self.zoom_level)
+		self.basemap.set_center([self.center_longitude, self.center_latitude])
+
+	def handler_button1_down(self,event):
+		# check if it is on hot spot, for loop should be full loop in order to make sure select the one on top
+		self.selected_spot = None
+		for spot in self.hot_spots:
+			pos = spot[0]
+			dis = (event.x-pos[0]) ** 2 + (event.y - pos[1]) ** 2
+			if dis < spot[2] ** 2:	# check distance
+				self.selected_spot = spot
+		
+		self.pos_of_move_from = [event.x,event.y]
+
+	def handler_button1_move(self,event):
+		if self.selected_spot != None:
+			self.selected_spot[0] = [event.x,event.y]
+			self.selected_spot[1] = self.get_coords(self.selected_spot[0])
+		else:
+			pass
+		#self.draw()
+		
+	def handler_button1_released(self,event):
+		if self.selected_spot != None:
+			self.selected_spot[0] = [event.x,event.y]
+			self.selected_spot[1] = self.get_coords(self.selected_spot[0])
+		else:	
+			movement = [event.x - self.pos_of_move_from[0], event.y - self.pos_of_move_from[1]]
+			self.move_center(movement)
+		
+		self.selected_spot = None
+		#self.draw()
+		
 		
 # Below is for helper functions
 
